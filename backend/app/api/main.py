253948f81505fc635411e4204bc9df089aaf0169
Giulia Tala <119086907+giutala@users.noreply.github.com>
2025-03-24 14:25:52 +0100

    Delete backend directory

--- backend/app/api/main.py
+++
@@ -1,7 +0,0 @@
-from fastapi import APIRouter
-from app.api.routes import stats, historical, gpt
-api_router = APIRouter()
-
-api_router.include_router(historical.router)
-api_router.include_router(stats.router)
-api_router.include_router(gpt.router)
